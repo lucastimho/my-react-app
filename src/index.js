5f9b05917c3a92862c7a512c7981f27e25d011c7
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-16 18:36:58 -0600

    adds multipage routes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const myfirstelement = <h1>Hello React!</h1>;
+function Hello() {
+  return <h2>Hello World!</h2>;
+}
 
-ReactDOM.render(myfirstelement, document.getElementById("root"));
+ReactDOM.render(myfirstelement, document.getElementById("mydiv"));
+ReactDOM.render(<Hello />, document.getElementById("root"));
