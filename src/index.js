952ea189e50cfa9266361ead1041fcdc634f8584
lucastimho <lucas.t.ho.lh@gmail.com>
2022-02-16 18:47:08 -0600

    sets up routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import Blogs from "./Blogs";
+import Contact from "./Contact";
+import NoPage from "./NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const myfirstelement = <h1>Hello React!</h1>;
 function Hello() {
